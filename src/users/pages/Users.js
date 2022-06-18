258405199a59edc2864ac2aa254e60ef0bf6a703
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-06-18 17:05:38 +0800

    Add more styles and components.

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,13 +1,13 @@
 import React from "react";
 import UserLists from "../components/UserLists";
+import sample from "../../images/sample.png";
 
 const Users = () => {
   const USER = [
     {
       id: "uid1",
       name: "Ryan Sacdalan",
-      image:
-        "https://www.pexels.com/photo/man-standing-in-front-of-window-12263516/",
+      image: sample,
       places: 5,
     },
   ];
