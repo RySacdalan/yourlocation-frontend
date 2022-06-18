ebb30bfdd1b6ed0260900ee85f15423284fd446e
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-06-18 17:57:19 +0800

    Create MainHeader components

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import {
 } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/navigation/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Users />
-        </Route>
-        <Route exact path="/places/new">
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
